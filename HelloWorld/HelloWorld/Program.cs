f855499abda6dad7505080f53a5319182a828c1b
Victor Paliari <victorrpaliari@gmail.com>
2023-08-22 18:15:54 -0300

    Era um Hello World, agora é uma Calculadora

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,8 +1,34 @@
 ﻿namespace HelloWorld {
     internal class Program { // classe principal
         static void Main(string[] args) { // método principal
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("A turma .NET 03 é top!");
+
+            int? n1, n2, soma;
+            int op = 1, 2, 3, 4, 5, 6;
+
+            Console.WriteLine("Escolha uma operação: ");
+            Console.WriteLine("1. Soma");
+            Console.WriteLine("2. Subtração");
+            Console.WriteLine("3. Divisão");
+            Console.WriteLine("4. Multiplicação");
+            Console.WriteLine("5. Porcentagem");
+
+            //if (Console.ReadLine(1)) {
+
+
+           // }
+
+
+           // else {
+            //    Console.WriteLine("Insira uma opção válida.");
+           // }
+            Console.WriteLine("Insira um número: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira outro número: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+
+            soma = (n1 + n2);
+            Console.WriteLine("A soma dos números é " + soma + "."); 
+           
         }
     }
 }
