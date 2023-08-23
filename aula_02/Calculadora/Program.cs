f8374cf5acd56a913d765b3b67e764f904793446
Victor Paliari <victorrpaliari@gmail.com>
2023-08-23 12:56:58 -0300

    Aula 02 - Exemplos

---
+++ aula_02/Calculadora/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.Serialization;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero1, numero2;
+
+            
+         
+            Console.WriteLine("Digite o primeiro número: ");
+            numero1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite o primeiro número: ");
+            numero2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine($"{numero1} + {numero2} = " + (numero1 + numero2)); // {} o colchetes serve para concatenar os valores
+            Console.WriteLine($"{numero1} - {numero2} = " + (numero1 - numero2));
+            Console.WriteLine($"{numero1} * {numero2} = " + (numero1 * numero2));
+            Console.WriteLine($"{numero1} / {numero2} = " + (numero1 / numero2));
+            Console.WriteLine($"{numero1} ^ {numero2} = " + Math.Pow(numero1, numero2)); // ^ 
+            Console.WriteLine($"Raiz quadrada de {numero1} = " + Math.Sqrt(numero1));
+
+
+        }
+    }
+}
