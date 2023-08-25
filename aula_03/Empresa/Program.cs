bca5feaee568ab97671d357b3f1d1dae25088d51
Victor Paliari <victorrpaliari@gmail.com>
2023-08-24 21:20:52 -0300

    Aula 03 - Exercícios resolvidos

--- aula_03/Empresa/Program.cs
+++ aula_03/Empresa/Program.cs
@@ -1,7 +1,79 @@
 ﻿namespace Empresa {
+       
     internal class Program {
+       
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+
+            string nomeColab;
+            float reajuste, salario;
+            int cargo;
+
+            Console.WriteLine("Bem vindo a empresa, por favor identifique-se:\n");
+            nomeColab = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Olá {nomeColab}, digite seu cargo: \n");
+
+            Console.WriteLine("1- Gerente\n");
+            Console.WriteLine("2- Vendedor \n");
+            Console.WriteLine("3- Supervisor\n");
+            Console.WriteLine("4-Motorista\n");
+            Console.WriteLine("5-Estoquista\n");
+            Console.WriteLine("6-Técnico de TI\n");
+            
+            cargo = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Digite seu salário atual: "); 
+            salario = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            switch (cargo) {
+
+                case 1:
+                    reajuste = salario * 10/100;
+                    Console.WriteLine("Gerentes possuem 10% de reajuste salarial.");
+                    Console.WriteLine($"Seu salário reajustado é igual a {salario + reajuste}.");
+
+                    break;
+
+                case 2:
+                    reajuste = salario * 7 / 100;
+                    Console.WriteLine("Vendedores possuem 7% de reajuste salarial.");
+                    Console.WriteLine($"Seu salário reajustado é igual a {salario + reajuste}.");
+
+                    break;
+
+                case 3:
+                    reajuste = salario * 9 / 100;
+                    Console.WriteLine("Supervisores possuem 9% de reajuste salarial.");
+                    Console.WriteLine($"Seu salário reajustado é igual a {salario + reajuste}.");
+
+                    break;
+                
+                case 4:
+                    reajuste = salario * 6 / 100;
+                    Console.WriteLine("Motoristas possuem 6% de reajuste salarial.");
+                    Console.WriteLine($"Seu salário reajustado é igual a {salario + reajuste}.");
+
+                    break;
+               
+                case 5:
+                    reajuste = salario * 5 / 100;
+                    Console.WriteLine("Estoquistas possuem 5% de reajuste salarial.");
+                    Console.WriteLine($"Seu salário reajustado é igual a {salario + reajuste}.");
+
+                    break;
+
+                case 6:
+                    reajuste = salario * 8 / 100;
+                    Console.WriteLine("Técnicos de TI possuem 8% de reajuste salarial.");
+                    Console.WriteLine($"Seu salário reajustado é igual a {salario + reajuste}.");
+
+                    break;
+
+
+            }
+
+
         }
     }
 }
--- aula_03/Lanchonete/Program.cs
+++ aula_03/Lanchonete/Program.cs
@@ -2,38 +2,81 @@
     internal class Program {
         static void Main(string[] args) {
 
-            int opcao, , op1, op2, op3, op4, op5, op6, quantidade,total; 
-           
+            int quantidade, opcao, valor;
+
             Console.WriteLine("Bem vindo a lanchonete!");
             Console.WriteLine("Esolha seu pedido: ");
             Console.WriteLine("1- Cachorro-quente R$10,00");
-            Console.WriteLine("2- X-Salada R$15,00");
-            Console.WriteLine("3- X-Bacon R$18,00");
-            Console.WriteLine("4- Bauru R$12,00");
-            Console.WriteLine("5- Refrigerante R$8,00");
-            Console.WriteLine("6- Suco de laranjaR$13,00");
+            Console.WriteLine("2- X-Salada        R$15,00");
+            Console.WriteLine("3- X-Bacon         R$18,00");
+            Console.WriteLine("4- Bauru           R$12,00");
+            Console.WriteLine("5- Refrigerante    R$8,00");
+            Console.WriteLine("6- Suco de laranja R$13,00");
 
-            opcao = Convert.ToInt32(Console.ReadLine());    
-            
+            opcao = Convert.ToInt32(Console.ReadLine());
+
             switch (opcao) {
 
                 case 1:
-                    int resposta;
-                    quantidade = Convert.ToInt32(Console.ReadLine());
-                    quantidade = quantidade + 1;
-                    Console.WriteLine("Você escolheu cachorro-quente, quer adicionar mais itens a seu pedido?");
-                    bool sim = resposta.ToLower() == sim;
-                    if (sim) {
 
-                    }
+                    Console.WriteLine("Quantidade: ");
+                    quantidade = Convert.ToInt32(Console.ReadLine());
+                    valor = quantidade * 10;
+                    Console.WriteLine($"Você escolheu ({quantidade}) cachorro(s)-quente(s), ");
+                    Console.WriteLine($"total = R${valor}");
                     
-                    break; 
+                    break;
+
+                case 2:
+
+                    Console.WriteLine("Quantidade: ");
+                    quantidade = Convert.ToInt32(Console.ReadLine());
+                    valor = quantidade * 15;
+                    Console.WriteLine($"Você escolheu ({quantidade}) X-salada(s), ");
+                    Console.WriteLine($"total = R${valor}");
+
+                    break;
+
+                case 3:
+
+                    Console.WriteLine("Quantidade: ");
+                    quantidade = Convert.ToInt32(Console.ReadLine());
+                    valor = quantidade * 18;
+                    Console.WriteLine($"Você escolheu ({quantidade}) X-bacon(s), ");
+                    Console.WriteLine($"total = R${valor}");
+
+                    break;
                 
-                
-                
-                case 2: 
-                    Console.WriteLine();
+                case 4:
+
+                    Console.WriteLine("Quantidade: ");
+                    quantidade = Convert.ToInt32(Console.ReadLine());
+                    valor = quantidade * 12;
+                    Console.WriteLine($"Você escolheu ({quantidade}) Bauru(s), ");
+                    Console.WriteLine($"total = R${valor}");
+
+                    break;
+
+                case 5:
+
+                    Console.WriteLine("Quantidade: ");
+                    quantidade = Convert.ToInt32(Console.ReadLine());
+                    valor = quantidade * 8;
+                    Console.WriteLine($"Você escolheu ({quantidade}) Refrigerante(s), ");
+                    Console.WriteLine($"total = R${valor}");
+
+                    break;
+
+                case 6:
+
+                    Console.WriteLine("Quantidade: ");
+                    quantidade = Convert.ToInt32(Console.ReadLine());
+                    valor = quantidade * 13;
+                    Console.WriteLine($"Você escolheu ({quantidade}) Suco(s) de laranja, ");
+                    Console.WriteLine($"total = R${valor}");
+
+                    break;
             }
+        }
     }
-    }
-    }
+}
