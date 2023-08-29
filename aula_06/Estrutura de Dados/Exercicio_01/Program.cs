21ac61b4922257de6ba47d5955d87c3a15a63d00
Victor Paliari <victorrpaliari@gmail.com>
2023-08-29 19:54:17 -0300

    Aula 06 - Estrutura de dados e Collections

---
+++ aula_06/Estrutura de Dados/Exercicio_01/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Exercicio_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+            Queue<string> fila = new Queue<string>();
+
+            int opcao = 0;
+            string cliente;
+
+            do 
+            {
+
+                Console.WriteLine("\n************************************************************");
+                Console.WriteLine("\n\t| 1 - Adicionar Cliente na Fila");
+                Console.WriteLine("\t| 2 - Listar Clientes");
+                Console.WriteLine("\t| 3 - Retirar Cliente da Fila");
+                Console.WriteLine("\t| 0 - Sair");
+                Console.WriteLine("\n************************************************************");
+                opcao = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:                       
+                        Console.Write("Digite o nome do cliente: ");
+                        cliente = (Console.ReadLine());
+                        fila.Enqueue(cliente);
+                        break;
+                    case 2:
+                        if (fila.Count == 0)
+                        {
+                            Console.WriteLine("Não tem ninguém na fila");
+                        break; 
+                        }
+                        else
+                        {
+                            foreach (var nome in fila)
+                            {
+                                Console.WriteLine(nome);
+                            }
+                        }
+                        break;
+                    case 3:
+                        if (fila.Count == 0)
+                        {
+                            Console.WriteLine("Não tem ninguém na fila");
+                            break;
+                        }
+                        else
+                        {
+                            fila.Dequeue();
+                            Console.WriteLine($"Cliente {fila.Peek()} retirado da fila.");
+                        }
+                        break;
+                    case 0:
+                        Console.Clear();
+                        Console.WriteLine("Programa finalizado, obrigado!");
+                        break;
+                }
+            }
+            while (opcao != 0);
+        }
+    }
+}
---
+++ aula_06/Estrutura de Dados/Exercicio_02/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace Exercicio_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<string> pilha = new Stack<string>();
+            int opcao;
+            string titulo;
+
+            while (true)
+            {
+
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.ForegroundColor = ConsoleColor.Black;
+                Console.WriteLine("***********************************************************************");
+                Console.WriteLine("     | 1 - Adicionar livro na pilha.                                   ");
+                Console.WriteLine("     | 2 - Listar todos os livros da pilha.                            ");
+                Console.WriteLine("     | 3 - Retirar livro da pilha.                                     ");
+                Console.WriteLine("     | 0 - Sair.                                                       ");
+                Console.WriteLine("***********************************************************************");
+                Console.WriteLine("Entre com a opção desejada:                                            ");
+                Console.ResetColor();
+
+                opcao = Convert.ToInt32(Console.ReadLine());
+                if (opcao == 0)
+                {
+                    Console.WriteLine("Operação finalizada, obrigado.");
+                    System.Environment.Exit(0);
+                }
+
+                switch (opcao)
+                {
+                    case 1:
+                        Console.WriteLine("Digite o titulo do livro: ");
+                        titulo = Console.ReadLine();
+
+                        pilha.Push(titulo);
+
+                        Console.WriteLine("Livro foi adicionado com sucesso. ");
+                        break;
+
+                    case 2:
+                        Console.WriteLine("Lsta de livros: ");
+                        foreach (var livro in pilha)
+                        {
+                            Console.WriteLine(livro);
+                        }
+                        break;
+
+                        case 3:
+                        if (pilha.Count == 0)
+                        {
+                            Console.WriteLine("A pilha está vazia");
+                        }
+                        else
+          
+                        pilha.Pop();
+                        Console.WriteLine("O livro foi retirado com sucesso. ");
+                        break;
+
+                    default: Console.WriteLine("Opção inválida.");
+                        break;
+                }
+            }
+        }
+    }
+}
