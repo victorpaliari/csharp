213f0e92b90eb81506ff10e3db1366dc68b4a1db
Victor Paliari <victorrpaliari@gmail.com>
2023-08-30 01:01:55 -0300

    Exercicios 3, 5 e 6 Resolvidos

--- aula_04/Exercicio_03/Program.cs
+++ aula_04/Exercicio_03/Program.cs
@@ -3,25 +3,37 @@
     internal class Program
     {
         static void Main(string[] args)
+
+
         {
             int idade; 
-            Console.WriteLine("Digite sua idade: ");
-            idade = Convert.ToInt32(Console.ReadLine());
+            int menor21 = 0;
+            int mais50 = 0;
          
-                while (idade > 0);
-            {
-                
-                
-                Console.WriteLine("Digite sua idade: ");
-                idade = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                     Console.WriteLine("Digite sua idade: ");
+                     idade = Convert.ToInt32(Console.ReadLine());
+
+                if (idade < 0)
+                {
+                    Console.WriteLine("Valor não corresponde ao intervalo...");
+                    break;
+                }
+
+                if (idade < 21)
+                {
+                    menor21++;
+                }  
+
+                if (idade > 50)
+                {
+                    mais50++;
+                }
+                        Console.WriteLine($"Menores de 21 anos: {menor21}");
+                        Console.WriteLine($"Maiores de 50 anos: {mais50}");
             }
-                    
-                Console.WriteLine("Idade inválida");
-
-
-
-
-
-        }
+        
+            }
     }
 }
---
+++ aula_04/Exercicio_05/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercicio_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros =0 ;
+            int positivos = 0;
+
+            do
+            {
+                Console.WriteLine("Digite um número: ");
+                numeros = Convert.ToInt32(Console.ReadLine());
+               
+                if (numeros > 0) 
+                positivos +=numeros;
+                
+            }
+            while (numeros != 0);
+                Console.WriteLine($"A soma dos positivos é igual a: {positivos} ");
+                
+
+            }
+
+    }
+}
