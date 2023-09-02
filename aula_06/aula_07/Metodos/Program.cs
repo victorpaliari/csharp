c2326c76ed86e94cb4fa34c17882a68c9688964c
Victor Paliari <victorrpaliari@gmail.com>
2023-09-02 19:50:50 -0300

    Exercícios resolvidos

---
+++ aula_06/aula_07/Metodos/Program.cs
@@ -0,0 +1,70 @@
+﻿
+
+using Metodos_02.Namespace01;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float numero1, numero2;
+
+            Console.WriteLine("Digite o primeiro número:");
+            numero1 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo número:");
+            numero2 = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine($"{numero1} + {numero2} = " + Somar(numero1, numero2));
+            Console.WriteLine($"{numero1} - {numero2} = " + Subtrair(numero1, numero2));
+            Console.WriteLine($"{numero1} * {numero2} = " + Multiplicar(numero1, numero2));
+            float divisao = Dividir(numero1, numero2);
+            Console.WriteLine(divisao == 0 ? $"Não existe divisão por zero{ numero1} / { numero2} = { divisao}";
+            Console.WriteLine($"{numero1} / {numero2} = " + Dividir(numero1, numero2));
+            Console.WriteLine($"{numero1} ^ {numero2} = " + Potencia(numero1, numero2));
+            Console.WriteLine($"{numero1} raiz de {numero2} = " + Raiz(numero1, numero2));
+            Dev();
+
+            Class2.MetodoPublico();
+
+        }
+        static float Somar (float numero1, float numero2)
+        {
+            return numero1 + numero2;
+        }
+
+        static float Subtrair (float numero1, float numero2)
+        {
+            return numero1 - numero2;
+        }
+
+        static float Multiplicar(float numero1, float numero2)
+        {
+            return numero1 * numero2;
+        }
+
+        static float Dividir (float numero1, float numero2)
+        {
+            if (numero2 > 0)
+                return numero1 / numero2;
+            else
+                return 0;
+                
+        }
+        static double Potencia(float numero1, float numero2)
+        {
+            return Math.Pow(numero1, numero2);
+        }
+
+        static double Raiz(float numero1, float numero2)
+        {
+            return Math.Sqrt(numero1);
+        }
+
+        static void Dev()
+        {
+            Console.WriteLine("Criado por: Victor Paliari");
+        }
+    }
+}
---
+++ aula_06/aula_07/Metodos_02/Program.cs
@@ -0,0 +1,45 @@
+﻿using Metodos_02.Namespace01;
+
+namespace Metodos_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MetodoPublico();
+            MetodoInternal();
+            MetodoPrivado();
+            MetodoProtegido();
+            Class1.MetodoPublico();
+            Class1.MetodoInternal();
+
+            //metodo protegido só é acessado se houver relação de herança
+            //Class1.MetodoProtegido();
+            //método privado só funciona dentro da classe onde ele foi criado
+            //Class1.MetodoPrivado();
+
+            Class2.MetodoPublico();
+            Class2.MetodoInternal();
+        }
+
+        public static void MetodoPublico()
+        {
+            Console.WriteLine($"Metodo Publico - Classe Program");
+        }
+
+        internal static void MetodoInternal()
+        {
+            Console.WriteLine($"Metodo Internal - Classe Program");
+        }
+
+        protected static void MetodoProtegido()
+        {
+            Console.WriteLine($"Metodo Protegido - Classe Program");
+        }
+
+        private static void MetodoPrivado ()
+        {
+            Console.WriteLine($"Metodo Privado - Classe Program");
+        }
+    }
+}
